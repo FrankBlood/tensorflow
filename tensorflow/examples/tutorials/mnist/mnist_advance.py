72504c64edf23a23fb7155863fc89c2bd77575e6
FrankBlood <1062947208@qq.com>
2016-07-11 13:54:37 +0800

    mnist advance
    run the advanced mnist tutorials.

--- tensorflow/examples/tutorials/mnist/mnist_advance.py
+++ tensorflow/examples/tutorials/mnist/mnist_advance.py
@@ -24,7 +24,7 @@ from __future__ import print_function
 
 # Import data
 from tensorflow.examples.tutorials.mnist import input_data
-
+import numpy as np
 import tensorflow as tf
 
 def weight_variable(shape):
@@ -64,18 +64,22 @@ b_conv1 = bias_variable([32])
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 print ("h_pool1 shape", tf.shape(h_pool1))
+
 W_conv2 = weight_variable([5, 5, 32, 64])
 b_conv2 = bias_variable([64])
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 print ("h_pool2 shape", tf.shape(h_pool2))
+
 W_fc1 = weight_variable([7 * 7 * 64, 1024])
 b_fc1 = bias_variable([1024])
 h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 print ("h_fc1 shape", tf.shape(h_fc1))
+
 keep_prob = tf.placeholder("float")
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+print ('dropout')
 
 W_fc2 = weight_variable([1024, 10])
 b_fc2 = bias_variable([10])
@@ -84,7 +88,8 @@ print ("y_conv shape", tf.shape(y_conv))
 
 cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-
+print (tf.shape(y_conv))
+print (tf.shape(y_))
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 
 
@@ -93,6 +98,12 @@ sess.run(tf.initialize_all_variables())
 for i in range(20000):
   batch = mnist.train.next_batch(50)
   if i%100 == 0:
+    print (type(batch[0]))
+    print (np.shape(batch[0]))
+    print (type(batch[0][0, 0]))
+    print (type(batch[1]))
+    print (np.shape(batch[1]))
+    print (type(batch[1][0, 0]))
     train_accuracy = accuracy.eval(feed_dict={
         x:batch[0], y_: batch[1], keep_prob: 1.0})
     print (i, train_accuracy)
